8b61e79adbf64930737d13f10de1242666742b5f
LMS Labs Deploy <deploy@lms-labs.com>
2026-08-23 12:15:43 +0000

    release: mod_contentcreator v13.76

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026082301;
+$plugin->version   = 2026082303;
 $plugin->requires  = 2023042400;
 $plugin->component = 'mod_contentcreator';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '13.75';
+$plugin->release   = '13.76';
